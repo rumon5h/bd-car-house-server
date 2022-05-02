5939ce609c47d46418e6a9247757352c24831433
rumon5 <mdrumonkhan48519@gmail.com>
2022-05-02 10:30:23 +0600

    adding heroku

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config()
 const port = process.env.PORT || 5000;
 const app = express();
 
@@ -7,6 +8,32 @@ const app = express();
 // Middleware
 app.use(express());
 app.use(cors());
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://BdCarHouse:6N9gpLMxzfHYtydL@carexportcompany.plhyy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect()
+        const database = client.db("cars");
+        const carsCollection = database.collection("car");
+
+        // GET CARS DATA
+        app.get('/cars', async(req, res) =>{
+        const query = {};
+        const cursor = carsCollection.find(query);
+        const cars = await cursor.toArray();
+        res.send(cars)
+        });
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir)
 
 
 app.get('/', (req, res) => {
