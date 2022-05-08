972f7f193d1ef2795e4bf2c6de68f47a75b2ba89
rumon5 <mdrumonkhan48519@gmail.com>
2022-05-08 07:02:53 +0600

    added delete api

--- index.js
+++ index.js
@@ -43,6 +43,14 @@ async function run() {
             const query = {_id: ObjectId(id)};
             const car = await carCollection.findOne(query);
             res.send(car)
+        });
+
+        // DELETE A CAR
+        app.delete('/car/:id', async(req,res) =>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result = await carCollection.deleteOne(query);
+            res.send(result)
         })
 
         // POST NEW CAR
